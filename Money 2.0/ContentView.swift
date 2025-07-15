cc25e2f116f37f8d818d0f85b6b2da97edebcf2e
Vladimir Zolotov <vova01gold@mail.ru>
2025-07-15 09:38:43 +0200

    Initial Commit

---
+++ Money 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Money 2.0
+//
+//  Created by Vladimir on 15.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Money 2.0/Money_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Money_2_0App.swift
+//  Money 2.0
+//
+//  Created by Vladimir on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Money_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
